1a981b38d6fa3c63ef700a7f9b4ed558f16adb51
mansurbek.sobirov <mansur.sobirov2007@gmail.com>
2025-09-04 14:03:19 +0500

    jndcja

--- jwt/urls.py
+++ jwt/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from jwt.views import *
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView, TokenVerifyView
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 urlpatterns = [
     path('get/', ActorAll.as_view()),
@@ -23,7 +21,6 @@ urlpatterns = [
     path('movies/year/<int:year>/', MoviesByYearView.as_view(), name='movies-by-year'),
     path('movies/range/', MoviesByYearRangeView.as_view(), name='movies-by-year-range'),
     path('movies/few-actors/', MoviesWithLessActorsView.as_view(), name='movies-few-actors'),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/', Login.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
